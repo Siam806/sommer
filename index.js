be6d8435c9df4aced78568f0fd9b4d842e1c639a
Siam806 <siamthedoom@gmail.com>
2023-10-20 01:14:01 -0700

    Hab int zu let geändert

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let petName = "Martha";
 const BABBEL = "laber";
 let wahr = false;
 
-for (int i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     console.log(i*i);
 }
 if (petName == "Martha" && BABBEL == "laber") {
